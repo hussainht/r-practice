9942f728862664e8320d5dda30da41d41e5ccf6c
hussain-hayat <hussainhayat9@gmail.com>
2019-08-12 02:47:54 +0500

    2nd commit via Rstudio

---
+++ xml-in-r.R
@@ -0,0 +1,22 @@
+library(XML)
+filename <- "files/simple.xml"
+doc <- xmlTreeParse(filename,useInternal = TRUE)
+rootnode <- xmlRoot(doc)
+xmlName(rootnode)
+
+names(rootnode)
+
+#Access parts of XML document
+rootnode[[1]]
+rootnode[[1]][[1]]
+
+#Programmatically extract parts of the file.
+xmlSApply(rootnode,xmlValue)
+
+# Extract data and make a data frame.
+rootnode
+names <- xpathSApply(rootnode,"//name",xmlValue)
+df <- data.frame(names)
+df$prices <- xpathSApply(rootnode,"//price",xmlValue)
+df$description <- xpathSApply(rootnode,"//description",xmlValue)
+df$calories <- xpathSApply(rootnode,"//calories",xmlValue)
